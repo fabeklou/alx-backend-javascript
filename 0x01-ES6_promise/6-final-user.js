3ccf2c4cd171b9692c0703835cfa647233135e66
Fabrice Eklou <fab.eklou@gmail.com>
2024-05-30 00:37:46 +0000

    fix: get rid of await

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const su = signUpUser(firstName, lastName);
   const up = uploadPhoto(fileName);
-  const responses = await Promise.allSettled(su, up);
-
-  return responses.map((res) => ({
-    status: res.status,
-    value: res.status === 'rejected' ? res.reason : res.value,
-  }));
+  return Promise.allSettled([su, up])
+    .then((res) => res);
 }
