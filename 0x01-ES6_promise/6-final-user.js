873b308de0baa586ae289ad0d908430b99b3a7e3
Fabrice Eklou <fab.eklou@gmail.com>
2024-05-30 02:27:34 +0000

    fix: get rid of async/await

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  (await promises).map((res) => ({
-    status: res.status,
-    value: res.status === 'rejected' ? res.reason : res.value,
-  }));
-  return promises;
+  return promises.then((res) => res);
 }
