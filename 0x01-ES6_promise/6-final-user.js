e45467b10373e44fb470373fdec12b1dfe675c14
Fabrice Eklou <fab.eklou@gmail.com>
2024-05-30 02:24:00 +0000

    refactor: use await promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const su = signUpUser(firstName, lastName);
-  const up = uploadPhoto(fileName);
-  return Promise.allSettled([su, up])
-    .then((res) => res);
+  const promises = Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  (await promises).map((res) => ({
+    status: res.status,
+    value: res.status === 'rejected' ? res.reason : res.value,
+  }));
+  return promises;
 }
