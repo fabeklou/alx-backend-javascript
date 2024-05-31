ad3aa34ede1515d20ae9ed94d0fee4f67a25c550
Fabrice Eklou <fab.eklou@gmail.com>
2024-05-31 00:38:44 +0000

    feat: implement an Airport class and his str description

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  toCustomString() {
+    return `${this._code}`;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
