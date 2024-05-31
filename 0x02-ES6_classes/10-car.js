56740699d8b594dbf492b63c1e6cf234dfab2241
Fabrice Eklou <fab.eklou@gmail.com>
2024-05-31 01:46:59 +0000

    feat: clone an object using a Symbol

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+const cloneSymbol = Symbol(123);
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  [cloneSymbol]() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
