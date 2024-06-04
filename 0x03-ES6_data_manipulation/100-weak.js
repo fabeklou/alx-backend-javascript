10a851a0789a952218019aa53c8198532eb9a297
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-04 16:40:14 +0000

    fix: logic in queryAPI, alws update weakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,6 +2,8 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   const frequency = weakMap.get(endpoint) || 0;
-  if (frequency > 4) throw new Error('Endpoint load is high');
   weakMap.set(endpoint, frequency + 1);
+  if (frequency >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
