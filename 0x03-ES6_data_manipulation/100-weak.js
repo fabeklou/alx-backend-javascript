fa4d9b472b82b066e4c59a94bbeac7d5678e73d9
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-04 16:42:31 +0000

    fix: change to frequency >= 4 after update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const frequency = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, frequency + 1);
-  if (frequency >= 5) {
+  if (frequency >= 4) {
     throw new Error('Endpoint load is high');
   }
 }
